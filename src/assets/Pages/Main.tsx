07f7b5154c8ab3c3da312729004171ce0dab8523
Nerz <115172452+Nerzzz@users.noreply.github.com>
2025-03-23 14:22:51 -0300

    Layout Change

--- src/assets/Pages/Main.tsx
+++ src/assets/Pages/Main.tsx
@@ -3,8 +3,8 @@ import cmdPng from "../images/Repeating_Command_Block_JE4_BE2.png";
 function Main() {
   return (
     <>
-      <section className='pt-[80px] pb-[50px] md:ml-[50px] h-[100vh] flex items-center'>
-        <div className='pl-[50px] pr-[50px] md:grid md:grid-cols-2 flex-1/2'>
+      <section className='pt-[80px] pb-[50px] md:ml-[50px] h-[100vh] flex'>
+        <div className='md:pl-[50px] md:pr-[50px] pl-[30px] pr-[30px] grid md:grid-cols-2 flex-1/2 items-center'>
           <div>
             <h1 className='font-capital md:text-[40pt]/[45pt] text-[20pt]/[25pt] font-bold text-white mb-[30px] border-l-1 pl-[20px] md:pt-[50px] md:pb-[25px] pb-[10px] pt-[25px] max-w-[600px]'>
               HELLO!<br />
--- src/assets/Pages/Works.tsx
+++ src/assets/Pages/Works.tsx
@@ -2,7 +2,7 @@ function Works() {
   return (
     <>
       <section className='pt-[80px] pb-[50px] md:ml-[50px] h-[100vh] flex'>
-        <div className="pl-[50px] pr-[50px] md:grid">
+        <div className="md:pl-[50px] md:pr-[50px] pl-[30px] pr-[30px] grid">
           <div>
             <h1 className='font-capital md:text-[40pt]/[45pt] text-[20pt]/[25pt] font-bold text-white mb-[30px] border-l-1 pl-[20px] md:pt-[50px] md:pb-[25px] pb-[10px] pt-[25px] max-w-[600px]'>
               MY WORKS & PROJECTS<br />
